fae136fe954b17ec9facc4a6646ba8b67b2dba8c
Samuel <samuelpeybernes33@gmail.com>
2022-02-24 16:09:47 +0100

    Back

--- merchex/listings/models.py
+++ merchex/listings/models.py
@@ -1,10 +1,18 @@
 from django.db import models
-
+from django.core.validators import MaxValueValidator, MinValueValidator
 
 class Band(models.Model):
     name = models.fields.CharField(max_length=100)
     biography = models.fields.CharField(max_length=1000) 
-   
+    year_formed = models.fields.IntegerField(
+    validators=[MinValueValidator(1900), MaxValueValidator(2021)]
+    )
+    class Genre(models.TextChoices):
+        HIP_HOP = 'HH'
+        SYNTH_POP = 'SP'
+        ALTERNATIVE_ROCK = 'AR'
+        HARD_ROCK = 'HR'
+    genre = models.fields.CharField(choices=Genre.choices, max_length=5)
     active = models.fields.BooleanField(default=True)
     official_homepage = models.fields.URLField(null=True, blank=True)
 
