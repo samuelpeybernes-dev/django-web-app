7ff572cb348a0ba7e0259dfdf3847425f0525194
Samuel <samuelpeybernes33@gmail.com>
2022-02-24 17:12:42 +0100

    Avant crud

--- merchex/listings/models.py
+++ merchex/listings/models.py
@@ -1,3 +1,4 @@
+from tkinter import SOLID
 from django.db import models
 from django.core.validators import MaxValueValidator, MinValueValidator
 
@@ -18,4 +19,12 @@ class Band(models.Model):
 
 class Title(models.Model):
     title = models.fields.CharField(max_length=100)
-
+    description = models.fields.CharField(max_length=1000)
+    sold = models.fields.BooleanField(default=True)
+    year = models.fields.IntegerField(null=True )
+    class Type(models.TextChoices):
+        RECORDS = 'RE'
+        CLOTHING = 'CL'
+        POSTERS = 'PO'
+        DIVERS = 'DI'
+    types = models.fields.CharField(choices=Type.choices, max_length=5)
