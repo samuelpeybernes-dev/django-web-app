31716e4661a216d1d4b8242096378c5365631755
Samuel <samuelpeybernes33@gmail.com>
2022-03-03 10:01:21 +0100

    ee

--- merchex/listings/views.py
+++ merchex/listings/views.py
@@ -4,11 +4,16 @@ from django.shortcuts import render
 from listings.models import Band
 from listings.models import Title
 
-def hello(request):
+def band_list(request):
     bands = Band.objects.all()
     return render(request, 
-            'listings/hello.html',
+            'listings/band_list.html',
             {'bands': bands})
+urlpatterns = [
+   ...
+   path('bands/', views.band_list),  # mise à jour du chemin et de la vue
+   ...
+]
 
 def about(request):
      return render(request, 
